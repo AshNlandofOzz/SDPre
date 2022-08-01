a1b07c9ba1fa844a36357751e30b2653e20b0623
AshNlandofOzz <ashleyosburn88@gmail.com>
2022-08-01 15:59:25 -0500

    finish Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Ashley Osburn";
+    int age = 34;
+    String state = "Texas";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
