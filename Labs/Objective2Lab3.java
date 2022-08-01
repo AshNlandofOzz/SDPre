9057f0adda9808d57a960a79234b8c7548000e66
AshNlandofOzz <ashleyosburn88@gmail.com>
2022-08-01 16:15:12 -0500

    finish Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int birthyear = 1988;
+    String month = "February";
+    boolean weekday, weekend;
+    weekday = false;
+    weekend = true;
+    System.out.println(birthyear);
+    System.out.println(month);
+    System.out.println(weekday);
+  }
+}
