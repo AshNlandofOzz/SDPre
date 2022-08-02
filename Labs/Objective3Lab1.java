c6d3b897694b3d571f8a1b10c0f2385e3fdf3756
AshNlandofOzz <ashleyosburn88@gmail.com>
2022-08-02 08:58:19 -0500

    finish Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,10 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    int num1 = 3;
+    int num2 = 5;
+    int num3 = 7;
+    int num4 = num1+num2+num3;
+    int average = num4/3;
+    System.out.println(average);
+  }
+}
